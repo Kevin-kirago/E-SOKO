f4e3d44ec84431c14dab48120699c96190138a17
Kevin <tkevinkiragu@gmail.com>
2020-03-03 23:27:16 +0300

    header video section

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -4,7 +4,14 @@ import "./home.styles.scss";
 const HomePage = () => {
 	return (
 		<div className="home">
-			<h1>Home</h1>
+			<div className="header">
+				<div className="video-container">
+					<video className="video-bg" autoPlay loop>
+						<source src="https://storage.coverr.co/videos/coverr-fashion-shopping--1563961262936?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6IjExNDMyN0NEOTRCMUFCMTFERTE3IiwiaWF0IjoxNTgzMjI4ODY2fQ.Qoq3IjeDqWb0eXu-98ZDnEtL_kFj-0mZpwCJ6GvR40U" />
+					</video>
+				</div>
+				<div className="content"></div>
+			</div>
 		</div>
 	);
 };
